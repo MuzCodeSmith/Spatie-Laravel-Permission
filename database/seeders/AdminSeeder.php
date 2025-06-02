8ec07adc4e366bd1b2ae36c257704bcbf7299e5a
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-06-02 13:33:12 +0530

    created a user

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
@@ -12,6 +14,11 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' =>Hash::make('password'),
+        ])->assignRole(['writer', 'admin']);        
     }
 }
